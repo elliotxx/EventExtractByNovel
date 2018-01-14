36b4f8df524f721bb2143532c1fd92f57a23f25a
windcode <951376975@qq.com>
2018-01-14 20:07:14 +0800

    初步可以对事件进行分类了，思路是提取主题词的词频为特征向量，再用SVM训练分类器

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import os
 import jieba
 import jieba.posseg as pseg
 import jieba.analyse
+from common import *
 
 # filename = 'test.txt'
 filename = '《盘龙》.txt'.decode('utf8').encode('gb2312')
 data_filename = 'data.txt'		# 字符串存储特征向量
-data_filename2 = 'data2.txt'	# unicode对应整型存储特征向量
+# data_filename2 = 'data2.txt'	# unicode对应整型存储特征向量
 
 # 设置主题词
 theme = [
@@ -25,6 +26,9 @@ event_type = {k:v.decode('utf8') for k,v in event_type.items()}
 
 # 自定义字典
 jieba.add_word('林雷', 15, 'n')
+# jieba.add_word('心中', 100, 'v')
+# for feature_tag in feature_tag_list:
+# 	jieba.add_word(feature_tag, 1)
 
 # 若特征数据集文件不存在，那么创建一个
 if not os.path.exists(data_filename):
@@ -32,9 +36,9 @@ if not os.path.exists(data_filename):
 	fp.close()
 
 # 若特征数据集文件不存在，那么创建一个
-if not os.path.exists(data_filename2):
-	fp = open(data_filename2,'w')
-	fp.close()
+# if not os.path.exists(data_filename2):
+# 	fp = open(data_filename2,'w')
+# 	fp.close()
 
 # 打开小说文本（数据源）
 fp = open(filename, 'rb')
@@ -52,48 +56,75 @@ for line in fp:
 		words.append(w.word)
 		flags.append(w.flag)
 
+
 	# 匹配触发词
-	isTrigger = False
+	# isTrigger = False
 	for i, w in enumerate(words):
 		if  w in theme and \
 			( (i+1 < len(words) and flags[i+1]=='v') or\
 			  (i+2 < len(words) and flags[i+2]=='v') ) and\
 			(i-1 >= 0 and 'v' not in flags[i-1]):
 			# 这句话包含主题词 且 主题词后面两个词其中之一为动词
-			if not isTrigger:
-				print line
-				isTrigger = True
-			if i-1 >= 0:
-				print words[i-1], flags[i-1]
-			print w, flags[i]
-			if i+1 < len(words):
-				print words[i+1], flags[i+1]
-			if i+2 < len(words):
-				print words[i+2], flags[i+2]
+			# 触发成功
+			# 构造特征向量
+			feature = [line.count(feature_tag) for feature_tag in feature_tag_list]
+			if not any(feature[:-1]):
+				continue
+
+			# 输出这句话
+			print line
+
+			# 记录是否第一次触发
+			# if not isTrigger:
+			# 	print line
+			# 	isTrigger = True
+
+			# 输出相邻词性
+			# if i-1 >= 0:
+			# 	print words[i-1], flags[i-1]
+			# print w, flags[i]
+			# if i+1 < len(words):
+			# 	print words[i+1], flags[i+1]
+			# if i+2 < len(words):
+			# 	print words[i+2], flags[i+2]
+
 			# 输出关键词
+			print 'Top 10 key:'
 			tags = jieba.analyse.extract_tags(line, topK=10)
-			# print tags
-			if len(tags)<10:
-				tags += ['']*(10-len(tags))
-			tags_str = ','.join(tags) 
-			print tags_str
-			tags_str2 = ','.join([ ''.join([str(ord(x)) for x in tag]) for tag in tags])
+			print ','.join(tags)
+			
+			print 'Word freq:'
+			print feature
+			# seg_list = jieba.cut(line)
+			# print ','.join(seg_list)
+			
+			# if len(tags)<10:
+			# 	tags += ['']*(10-len(tags))
+			# tags_str = ','.join(tags) 
+			# print tags_str
+			# tags_str2 = ','.join([ ''.join([str(ord(x)) for x in tag]) for tag in tags])
 			# print tags_str2
 
 			# 构造特征数据集
+			# 构造特征向量，存入本地
+
+
 			label = raw_input()
 			if label != '' and int(label) in range(1,len(event_type)+1):
-				label = int(label)
 				# 是预定事件类别
-				# 构造特征向量，存入本地
+				label = int(label)
+				feature.append(label)
+				print 'Feature:'
+				print feature
+				print ''
 				ft = open(data_filename, 'a')
 				# ft.write('%s,%s\n'%(tags_str.encode('utf8'), event_type[label].encode('utf8')))
-				ft.write('%s,%s\n'%(tags_str.encode('utf8'), str(label)))
+				ft.write('%s\n'%(','.join([str(x) for x in feature])))
 				ft.close()
-				ft = open(data_filename2, 'a')
-				# ft.write('%s,%s\n'%(tags_str2, event_type[label].encode('utf8')))
-				ft.write('%s,%s\n'%(tags_str2, str(label)))
-				ft.close()
+				# ft = open(data_filename2, 'a')
+				# # ft.write('%s,%s\n'%(tags_str2, event_type[label].encode('utf8')))
+				# ft.write('%s,%s\n'%(tags_str2, str(label)))
+				# ft.close()
 			break
 
 	# 事件元素识别
--- train_model.py
+++ train_model.py
@@ -1,27 +1,124 @@
 #coding=utf8
+import os
+import jieba
+import jieba.posseg as pseg
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 import numpy as np
+from common import *
+
 # 读取
-path = 'data2.txt'  # 数据文件路径
+path = 'data.txt'  # 数据文件路径
 data = np.loadtxt(path, dtype=float, delimiter=',')	#, converters={4: iris_type})
 # print data
 
 # 分割为训练集和测试集
-x, y = np.split(data, (10,), axis=1)
+x, y = np.split(data, (16,), axis=1)
 # x = x[:, :2]
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, train_size=0.6)
-print x_train, y_train
+# print x_train
+# print y_train
 
 # 训练svm分类器
 # clf = svm.SVC(C=0.1, kernel='linear', decision_function_shape='ovr')
-clf = svm.SVC(C=0.8, kernel='rbf', gamma=20, decision_function_shape='ovr')
+clf = svm.SVC(C=0.8, kernel='rbf', gamma=20, decision_function_shape='ovr', probability=True)
 clf.fit(x_train, y_train.ravel())
 
 # 计算svc分类器的准确率
+print 'Train accuracy:'
 print clf.score(x_train, y_train)  # 精度
 y_hat = clf.predict(x_train)
-show_accuracy(y_hat, y_train, '训练集')
+# show_accuracy(y_hat, y_train, '训练集')
+print 'Test accuracy:'
 print clf.score(x_test, y_test)
+# print x_test
 y_hat = clf.predict(x_test)
-show_accuracy(y_hat, y_test, '测试集')
+print 'Predict ans:'
+print y_hat
+# show_accuracy(y_hat, y_test, '测试集')
+
+
+
+
+filename = '《盘龙》.txt'.decode('utf8').encode('gb2312')
+data_filename = 'data.txt'		# 字符串存储特征向量
+# data_filename2 = 'data2.txt'	# unicode对应整型存储特征向量
+
+# 设置主题词
+theme = [
+'林雷',
+]
+theme = [w.decode('utf8') for w in theme]
+
+# 设置事件类别
+event_type = {
+1:'修炼',
+2:'对话',
+3:'心理活动',
+}
+event_type = {k:v.decode('utf8') for k,v in event_type.items()}
+
+# 自定义字典
+# jieba.add_word('林雷', 15, 'n')
+# jieba.add_word('心中', 100, 'v')
+# for feature_tag in feature_tag_list:
+# 	jieba.add_word(feature_tag, 1)
+
+# 打开小说文本（数据源）
+fp = open(filename, 'rb')
+for line in fp:
+	# 提取每句话
+	line = line.decode('utf8').strip()
+	if len(line) == 0:
+		continue
+
+	# 对每句话进行词性标注
+	res = pseg.cut(line)
+	words = []
+	flags = []
+	for w in res:
+		words.append(w.word)
+		flags.append(w.flag)
+
+
+	# 匹配触发词
+	# isTrigger = False
+	for i, w in enumerate(words):
+		if  w in theme and \
+			( (i+1 < len(words) and flags[i+1]=='v') or\
+			  (i+2 < len(words) and flags[i+2]=='v') ) and\
+			(i-1 >= 0 and 'v' not in flags[i-1]):
+			# 这句话包含主题词 且 主题词后面两个词其中之一为动词
+			# 触发成功
+			# 构造特征向量
+			feature = [line.count(feature_tag) for feature_tag in feature_tag_list]
+			if not any(feature[:-1]):
+				continue
+			feature = np.array([[float(x) for x in feature]])
+
+			# 筛掉预测率低于8%的结果
+			proba_list = clf.predict_proba(feature)
+			if not (any([x>0.7 for x in proba_list[0]])):
+				break
+
+			# 输出这句话
+			print line
+			print 'Word freq:'
+			print feature
+			print 'Predict res:'
+			y_hat = clf.predict(feature)
+			print event_type[int(y_hat[0])]
+			print proba_list
+
+			os.system('pause')
+			print ''
+			break
+
+
+
+	# 事件元素识别
+	
+
+	# if isTrigger:
+	
+fp.close()
